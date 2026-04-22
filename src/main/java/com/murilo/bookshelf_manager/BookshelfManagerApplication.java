0a76cd7b9737fd482fc21d11391cfa6886f97bf6
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-04-22 18:14:41 -0300

    initial commit

---
+++ src/main/java/com/murilo/bookshelf_manager/BookshelfManagerApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.bookshelf_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfManagerApplication.class, args);
+	}
+
+}
