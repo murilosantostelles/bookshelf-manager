28ba76030bfd15f93292e2bf809461cff214b23a
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-05-08 19:57:43 -0300

    feat: implementa AutorService e AutorController

---
+++ src/main/java/com/murilo/bookshelf_manager/controller/AutorController.java
@@ -0,0 +1,59 @@
+package com.murilo.bookshelf_manager.controller;
+
+import com.murilo.bookshelf_manager.dto.autor.AutorRequestDTO;
+import com.murilo.bookshelf_manager.dto.autor.AutorResponseDTO;
+import com.murilo.bookshelf_manager.entity.Autor;
+import com.murilo.bookshelf_manager.service.AutorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/autores")
+@RequiredArgsConstructor
+public class AutorController {
+
+    private final AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<AutorResponseDTO> createAutor(@Valid @RequestBody AutorRequestDTO dto){
+        AutorResponseDTO autorCriado = autorService.createAutor(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(autorCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorResponseDTO>> findAllAutores(){
+        List<AutorResponseDTO> autoresEncontrados = autorService.findAllAutores();
+        return ResponseEntity.ok().body(autoresEncontrados);
+    }
+
+    @GetMapping("/nome")
+    public ResponseEntity<List<AutorResponseDTO>> findByNome(@RequestParam String nome){
+        List<AutorResponseDTO> autoresEncontrados = autorService.findByNome(nome);
+        return ResponseEntity.ok().body(autoresEncontrados);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorResponseDTO> findById(@PathVariable Long id){
+        AutorResponseDTO autor = autorService.findById(id);
+        return ResponseEntity.ok().body(autor);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AutorResponseDTO> updateAutor(@PathVariable Long id, @Valid @RequestBody AutorRequestDTO dto){
+        return ResponseEntity.ok().body(autorService.updateAutor(id, dto));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAutor(@PathVariable Long id){
+        autorService.deleteAutor(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/murilo/bookshelf_manager/service/AutorService.java
+++ src/main/java/com/murilo/bookshelf_manager/service/AutorService.java
@@ -1,13 +1,75 @@
 package com.murilo.bookshelf_manager.service;
 
+import com.murilo.bookshelf_manager.dto.autor.AutorRequestDTO;
+import com.murilo.bookshelf_manager.dto.autor.AutorResponseDTO;
+import com.murilo.bookshelf_manager.entity.Autor;
 import com.murilo.bookshelf_manager.repository.AutorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AutorService {
 
     private final AutorRepository autorRepository;
+
+    //post
+    public AutorResponseDTO createAutor(AutorRequestDTO dto){
+        Autor autor = new Autor();
+        autor.setNome(dto.nome());
+        Autor autorSalvo = autorRepository.save(autor);
+        return new AutorResponseDTO(
+                autorSalvo.getId(),
+                autorSalvo.getNome()
+        );
+    }
+
+
+    //get
+    public List<AutorResponseDTO> findAllAutores(){
+        return autorRepository.findAll()
+                .stream()
+                .map(autor -> new AutorResponseDTO(autor.getId(),autor.getNome()))
+                .toList();
+    }
+
+
+    public List<AutorResponseDTO> findByNome(String nome){
+        return autorRepository.findByNomeContainingIgnoreCase(nome)
+                .stream()
+                .map(autor -> new AutorResponseDTO(autor.getId(), autor.getNome()))
+                .toList();
+    }
+
+    public AutorResponseDTO findById(Long id){
+        Autor autorEncontrado = autorRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Autor não encontrado"));
+
+        return new AutorResponseDTO(autorEncontrado.getId(), autorEncontrado.getNome());
+    }
+
+    //put, patch
+    public AutorResponseDTO updateAutor(Long id, AutorRequestDTO dto){
+        Autor autor = autorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Autor não encontrado"));
+
+        autor.setNome(dto.nome());
+
+        Autor autorAtualizado = autorRepository.save(autor);
+
+        return new AutorResponseDTO(autorAtualizado.getId(),
+                autorAtualizado.getNome());
+    }
+
+    //delete
+    public void deleteAutor(Long id){
+        Autor autor = autorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Autor não encontrado"));
+
+        autorRepository.delete(autor);
+    }
+
 
 }
